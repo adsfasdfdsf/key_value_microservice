095efee4843363290c59475963062795eab275bb
osminohzka <mariampozdnyakova@gmail.com>
2026-05-14 11:51:27 +0300

    grpc multiclient added

--- storage-node/internal/transport/grpc/multiclient.go
+++ storage-node/internal/transport/grpc/multiclient.go
@@ -1,2 +1,39 @@
 package grpc
 
+import (
+	"context"
+	"fmt"
+
+	"gitlab.com/adsfasdfdsf-group/key-value-service/pkg/logger"
+	"golang.org/x/sync/errgroup"
+)
+
+
+type Multiclient struct {
+	clients []Client
+}
+
+func New() *Multiclient{
+	return &Multiclient{
+		clients: make([]Client, 0),
+	}
+}
+
+func (mc *Multiclient) AddClient(cl Client) {
+	mc.clients = append(mc.clients, cl)
+}
+
+func (mc *Multiclient) AppendValue(ctx context.Context, key, value string) error {
+	eg := errgroup.Group{}
+	log := logger.GetLogger(ctx)
+	for _, i := range mc.clients{
+		eg.Go(func() error {
+			success, err := i.AppendValue(ctx, key, value)
+			if err != nil || success == false{
+				log.Error(ctx, fmt.Sprintf("Append value via grpc on server %v failed", i.address))
+			}
+			return err
+		})
+	}
+	return eg.Wait()
+}
