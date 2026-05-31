8baf35c1f0f5fbae700c84994d8b3431faa131e5
sadfad <alfhcwl@yandex.ru>
2026-05-31 04:53:20 +0300

    new looger levels

--- storage-node/pkg/logger/logger.go
+++ storage-node/pkg/logger/logger.go
@@ -14,11 +14,23 @@ const (
 type Logger interface {
 	Info(ctx context.Context, msg string, fields ...zap.Field)
 	Error(ctx context.Context, msg string, fields ...zap.Field)
+	Warn(ctx context.Context, msg string, fields ...zap.Field)
+	Debug(ctx context.Context, msg string, fields ...zap.Field)
 }
 
 type logger struct {
 	serviceName string
 	logger      *zap.Logger
+}
+
+func (l logger) Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String(ServiceName, l.serviceName))
+
+	if ctx.Value(RequestId) != nil {
+		fields = append(fields, zap.String(RequestId, ctx.Value(RequestId).(string)))
+	}
+
+	l.logger.Debug(msg, fields...)
 }
 
 func (l logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
@@ -29,6 +41,16 @@ func (l logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
 	}
 
 	l.logger.Info(msg, fields...)
+}
+
+func (l logger) Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String(ServiceName, l.serviceName))
+
+	if ctx.Value(RequestId) != nil {
+		fields = append(fields, zap.String(RequestId, ctx.Value(RequestId).(string)))
+	}
+
+	l.logger.Warn(msg, fields...)
 }
 
 func (l logger) Error(ctx context.Context, msg string, fields ...zap.Field) {
