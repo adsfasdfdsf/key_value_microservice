87bf77afe0612a8cd99864b6e51d6d7bc33c6e7e
adsfsdf <->
2026-08-21 23:18:39 +0300

    simple backend ready

--- auth/cmd/app/main.go
+++ auth/cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"auth/internal/storagenode"
 	userserver "auth/internal/transport/UserServer"
 	"auth/pkg/logger"
 	"auth/pkg/storage/userrepo"
@@ -15,7 +16,7 @@ func main() {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, logger.LoggerKey, logger.New(serviceName))
 	mainLogger := logger.GetLogger(ctx)
-	s := userserver.New("1128", userrepo.New())
+	s := userserver.New("1128", userrepo.New(), storagenode.NewSimpleStorage())
 	mainLogger.Info(ctx, "serverStarted")
 	if s.Run(ctx) != nil {
 	}
